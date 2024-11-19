2b9e1baec4f666923e3d23a08edaabfce8e6a5cc
clayts <clayton.spicer@gmail.com>
2024-11-19 08:52:35 +0000

    add support for automatic brightness

--- hosts/aeon/default.nix
+++ hosts/aeon/default.nix
@@ -1,9 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-scan.nix
     ../../hardware/standard
     ../../hardware/fingerprint-reader.nix
     ../../hardware/meteor-lake-camera.nix
+    ../../hardware/iio-sensors.nix
 
     ../../os
     ../../desktop
