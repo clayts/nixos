94fbadc28d495c41cf2892cd13f2683dcf6ab87c
clayts <clayton.spicer@gmail.com>
2024-11-22 04:20:54 +0000

    remove unnecessary vscode extensions

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = inputs: {
     nixosConfigurations =
