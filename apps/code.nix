8de1146fe4e0880ae9a48f4084609b1fc7f08042
clayts <clayton.spicer@gmail.com>
2024-11-22 04:20:25 +0000

    switched zed to FHS

--- apps/code.nix
+++ apps/code.nix
@@ -1,15 +1,47 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    zed-editor
+  environment.systemPackages = let
+    zed-editor-fhs = pkgs.buildFHSUserEnv {
+      name = "zed-editor";
+      targetPkgs = pkgs:
+        with pkgs; [
+          openssl
+          alejandra
+          nixd
+          zed-editor
+        ];
+      runScript = "zeditor";
+    };
 
-    # Nix
-    alejandra
-    # nil
-    nixd
+    zed-editor-desktop-item = pkgs.makeDesktopItem {
+      name = "zed";
+      desktopName = "Zed";
+      exec = "${zed-editor-fhs}/bin/zed-editor %F"; # Use the FHS wrapper
+      icon = "${pkgs.zed-editor}/share/icons/hicolor/512x512/apps/zed.png"; # Direct path to the icon
+      comment = "High-performance, multiplayer code editor";
+      categories = ["Development" "TextEditor"];
+      mimeTypes = [
+        "text/plain"
+        "text/x-markdown"
+        "text/x-script.python"
+        "text/x-rust"
+        "text/javascript"
+        "application/json"
+        "text/x-c"
+        "text/x-c++"
+      ];
+      terminal = false;
+      type = "Application";
+      genericName = "Text Editor";
+      startupNotify = true;
+    };
+  in
+    with pkgs; [
+      zed-editor-fhs
+      zed-editor-desktop-item
 
-    # Git
-    gitg
-    git
-    gh
-  ];
+      # Git
+      gitg
+      git
+      gh
+    ];
 }
