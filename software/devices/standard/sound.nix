0ca2442ec128b8c8d7a59fac746d317e1af3934b
clayts <clayton.spicer@gmail.com>
2024-11-27 17:01:24 +0000

    fix camera

--- software/devices/standard/sound.nix
+++ software/devices/standard/sound.nix
@@ -1,15 +1,15 @@
 {...}: {
   security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
-  # Fix sound on Noita
-  services.pipewire.enable = false;
-  services.gnome.gnome-remote-desktop.enable = false;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  # };
+  # # Fix sound on Noita
+  # services.pipewire.enable = false;
+  # services.gnome.gnome-remote-desktop.enable = false;
+  # hardware.pulseaudio.enable = true;
+  # hardware.pulseaudio.support32Bit = true;
 }
