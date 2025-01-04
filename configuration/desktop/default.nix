9cfde4eb8b7e6c350ddb5d112b76c6f670c273cf
clayts <clayton.spicer@gmail.com>
2025-01-04 02:40:13 +0000

    cleanup

--- configuration/desktop/default.nix
+++ configuration/desktop/default.nix
@@ -33,8 +33,7 @@ in {
     adw-gtk3
 
     ## Fonts
-    # (nerdfonts.override {fonts = ["CascadiaCode"];}) # <= 24.11
-    nerd-fonts.caskaydia-cove # > 24.11
+    nerd-fonts.caskaydia-cove
     noto-fonts
   ];
 
