33a73fd741ce102fba5b2bc5ef92f2ad6e35db17
clayts <clayton.spicer@gmail.com>
2025-04-10 05:05:57 +0100

    Improve hardware configuration naming and comments

--- hardware/fixes.nix
+++ hardware/fixes.nix
@@ -1,28 +1,31 @@
 {pkgs, ...}: {
+  # Bluetooth
   # hardware.firmware = let
-  #   linux-firmware-c1a77 = pkgs.linux-firmware.overrideAttrs (oldAttrs: {
+  #   linux-firmware-git = pkgs.linux-firmware.overrideAttrs (oldAttrs: {
   #     src = pkgs.fetchgit {
   #       url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
   #       rev = "c1a774f36657e30e28a83002a8ebf5add44e02aa";
   #       sha256 = "sha256-aQdEl9+7zbNqWSII9hjRuPePvSfWVql5u5TIrGsa+Ao=";
   #     };
   #   });
-  # in [linux-firmware-c1a77];
+  # in [linux-firmware-git];
 
+  # Sound
   environment.sessionVariables = let
-    alsa-ucm-conf-30989 = pkgs.alsa-ucm-conf.overrideAttrs (oldAttrs: {
+    alsa-ucm-conf-git = pkgs.alsa-ucm-conf.overrideAttrs (oldAttrs: {
       src = pkgs.fetchFromGitHub {
         owner = "alsa-project";
         repo = "alsa-ucm-conf";
-        rev = "30989bd0c2aa3f9f4b6f5e393397b39678717f45"; # or specific commit/tag
+        rev = "30989bd0c2aa3f9f4b6f5e393397b39678717f45";
         hash = "sha256-cFYEsavUeD6ZyZ/UqyjZnOcSJuOaSBe6sqEH2wOQddc=";
       };
     });
   in {
-    ALSA_CONFIG_UCM2 = "${alsa-ucm-conf-30989}/share/alsa/ucm2";
+    ALSA_CONFIG_UCM2 = "${alsa-ucm-conf-git}/share/alsa/ucm2";
   };
 
-  environment.etc."libinput/local-overrides.quirks".text = pkgs.lib.mkForce ''
+  # Touchpad
+  environment.etc."libinput/local-overrides.quirks".text = ''
     # The ThinkPad X9 15 Gen 1 Forcepad touchpad is not
     # detected as a pressure pad
     [Lenovo ThinkPad X9 15 Gen 1]
