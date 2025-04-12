74b49be2ad9b78a35338ca5172c9972bb01bbef5
clayts <clayton.spicer@gmail.com>
2025-04-12 04:17:58 +0100

    Add scripts for HTML and PDF document conversion

--- software/scripts.nix
+++ software/scripts.nix
@@ -1,9 +1,13 @@
 {pkgs, ...}: {
-  environment.systemPackages = [
-    (map (name:
+  environment.systemPackages =
+    map (name:
       pkgs.writeScriptBin
       (builtins.baseNameOf (pkgs.lib.removeSuffix ".sh" name))
       (builtins.readFile (./scripts + "/${name}")))
-    (builtins.attrNames (builtins.readDir ./scripts)))
-  ];
+    (builtins.attrNames (builtins.readDir ./scripts))
+    ++ [
+      (pkgs.writeScriptBin "html2pdf" "${pkgs.python313Packages.weasyprint}/bin/weasyprint $1 $2")
+      (pkgs.writeScriptBin "md2html" "${pkgs.pandoc}/bin/pandoc $1 -o $2")
+      (pkgs.writeScriptBin "md2pdf" "(echo '<head><meta charset=\"utf-8\" /></head>' && md2html $1 -) | html2pdf - $2")
+    ];
 }
