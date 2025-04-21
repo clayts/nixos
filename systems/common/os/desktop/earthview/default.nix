48b512c58deb221273a3be9d8185573de49f567b
clayts <clayton.spicer@gmail.com>
2025-04-21 19:28:38 +0100

    update

--- systems/common/os/desktop/earthview/default.nix
+++ systems/common/os/desktop/earthview/default.nix
@@ -1,5 +1,54 @@
-{pkgs, ...}: {
-  systemd.timers."wallpaper-switcher" = {
+{pkgs, ...}: let
+  earthview = pkgs.writeShellScriptBin "earthview" ''
+    usage() {
+        echo "$0 downloads a random EarthView (https://g.co/ev) image."
+        echo "Usage: $0 [-o output_file]"
+        echo "Options:"
+        echo "  -o FILE    Specify output image file (default: <image id>.jpeg)"
+        echo "  --help/-h  Show this help message"
+    }
+
+    image=""
+    while getopts "o:" opt; do
+        case $opt in
+            o) image="$OPTARG" ;;
+            h) usage && exit 1 ;;
+            ?) usage && exit 1 ;;
+        esac
+    done
+
+    id=$(${pkgs.jq}/bin/jq -r '.[]' ${./image-ids.json} | shuf -n 1)
+
+    if [ -z $id ];
+    then
+      exit 1
+    fi
+
+    if [ -z $image ];
+    then
+        image="$id.jpeg"
+    fi
+
+    dir=$(mktemp -d)
+
+    ${pkgs.curl}/bin/curl -s "https://www.gstatic.com/prettyearth/assets/data/v3/$id.json" -o $dir/data.json
+
+    read -r latitude longitude elevation country < <(${pkgs.jq}/bin/jq -r '.lat, .lng, .elevation, .geocode.country' $dir/data.json | tr '\n' ' ')
+    url="https://maps.google.com/?q=$latitude,$longitude"
+
+    ${pkgs.jq}/bin/jq -r '.dataUri' $dir/data.json | sed 's/^data:image\/jpeg;base64,//' | base64 -d > "$image"
+
+    echo """ID:        $id
+    Country:   $country
+    Latitude:  $latitude
+    Longitude: $longitude
+    Elevation: $elevation
+    Map URL:   $url"""
+
+    rm -Rf $dir
+  '';
+in {
+  systemd.timers."earthview-wallpaper" = {
     enable = true;
     wantedBy = ["timers.target"];
     timerConfig = {
@@ -8,24 +57,20 @@
     };
   };
 
-  systemd.services."wallpaper-switcher" = {
+  systemd.services."earthview-wallpaper" = {
     script = ''
-      echo "Fetching index"
-      baseURL="https://raw.githubusercontent.com/clayts/Wallpapers/master"
-      indexPath="index.txt"
-      index=($(${pkgs.curl}/bin/curl -s "$baseURL/$indexPath"))
-      echo "Index contains ''${#index[@]} images"
-      path="$(printf "%s\n" "''${index[@]}" | shuf -n1)"
-      if [ "$path" = "" ]
-      then
-          echo "Failed"
-          exit 1
-      fi
-      echo "Fetching $path"
-      temp=$(mktemp) && chmod +r $temp &&
+      dir="/var/lib/earthview-wallpaper"
+      image_tmp=$(mktemp) &&
+      info_tmp=$(mktemp) &&
+      chmod +r $image_tmp &&
+      chmod +r $info_tmp &&
       (
-          ${pkgs.curl}/bin/curl -s "$baseURL/$path" > $temp &&
-          mv $temp /tmp/wallpaper.jpg && echo "Wrote /tmp/wallpaper.jpg"
+          echo "Fetching wallpaper:" &&
+          mkdir -p $dir &&
+          ${earthview}/bin/earthview -o $image_tmp > $info_tmp &&
+          cat $info_tmp &&
+          mv $image_tmp $dir/wallpaper.jpeg &&
+          mv $info_tmp $dir/wallpaper-info.txt
       ) || (
           rm $temp && echo "Failed" && exit 1
       )
@@ -38,48 +83,6 @@
   };
 
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "earthview" ''
-      usage() {
-          echo "$0 downloads a random EarthView (https://g.co/ev) image."
-          echo "Usage: $0 [-o output_file]"
-          echo "Options:"
-          echo "  -o FILE    Specify output image file (default: <image id>.jpeg)"
-          echo "  --help/-h  Show this help message"
-      }
-
-      image=""
-      while getopts "o:" opt; do
-          case $opt in
-              o) image="$OPTARG" ;;
-              h) usage && exit 1 ;;
-              ?) usage && exit 1 ;;
-          esac
-      done
-
-      id=$(jq -r '.[]' ${./image-ids.json} | shuf -n 1)
-
-      if [ -z $image ];
-      then
-          image="$id.jpeg"
-      fi
-
-      dir=$(mktemp -d)
-
-      curl -s "https://www.gstatic.com/prettyearth/assets/data/v3/$id.json" -o $dir/data.json
-
-      read -r latitude longitude elevation country < <(jq -r '.lat, .lng, .elevation, .geocode.country' $dir/data.json | tr '\n' ' ')
-      url="https://maps.google.com/?q=$latitude,$longitude"
-
-      echo """ID:        $id
-      Country:   $country
-      Latitude:  $latitude
-      Longitude: $longitude
-      Elevation: $elevation
-      Map URL:   $url"""
-
-      jq -r '.dataUri' $dir/data.json | sed 's/^data:image\/jpeg;base64,//' | base64 -d > "$image"
-
-      rm -Rf $dir
-    '')
+    earthview
   ];
 }
