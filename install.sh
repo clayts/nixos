ccb52c27919ea1f1cece790c764b09a1aee06cab
clayts <clayton.spicer@gmail.com>
2025-04-24 19:40:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,73 +42,68 @@ else
 	}
 
 	""" > $HOSTNAME/default.nix
+
+	echo "Generating disks.nix and hardware.nix..."
+	sudo nixos-generate-config --show-hardware-config --no-filesystems > $HOSTNAME/hardware.nix
+
+	SWAP_SIZE="$(free -m | awk '/^Mem:/{print $2 * 2}')M"
+
+	echo """
+
+	{inputs, ...}: {
+	  imports = [
+	    inputs.disko.nixosModules.disko
+	  ];
+
+	  disko.devices = {
+	    disk = {
+	      main = {
+	        device = \"$DISK\";
+	        type = \"disk\";
+	        content = {
+	          type = \"gpt\";
+	          partitions = {
+	            boot = {
+	              type = \"EF00\";
+	              size = \"1G\";
+	              content = {
+	                type = \"filesystem\";
+	                format = \"vfat\";
+	                mountpoint = \"/boot\";
+	                mountOptions = [\"umask=0077\"];
+	              };
+	            };
+	            swap = {
+	              size = \"$SWAP_SIZE\";
+	              content = {
+	                type = \"swap\";
+	                discardPolicy = \"both\";
+	                resumeDevice = true;
+	              };
+	            };
+	            root = {
+	              size = \"100%\";
+	              content = {
+	                type = \"filesystem\";
+	                format = \"ext4\";
+	                mountpoint = \"/\";
+	              };
+	            };
+	          };
+	        };
+	      };
+	    };
+	  };
+	}
+
+
+	""" > $HOSTNAME/disks.nix
+
+	git add $HOSTNAME
 fi
-
-
-echo "Rescanning hardware..."
-sudo nixos-generate-config --show-hardware-config --no-filesystems > $HOSTNAME/hardware.nix
-
-SWAP_SIZE="$(free -m | awk '/^Mem:/{print $2 * 2}')M"
-
-echo ""
-echo """
-
-{inputs, ...}: {
-  imports = [
-    inputs.disko.nixosModules.disko
-  ];
-
-  disko.devices = {
-    disk = {
-      main = {
-        device = \"$DISK\";
-        type = \"disk\";
-        content = {
-          type = \"gpt\";
-          partitions = {
-            boot = {
-              type = \"EF00\";
-              size = \"1G\";
-              content = {
-                type = \"filesystem\";
-                format = \"vfat\";
-                mountpoint = \"/boot\";
-                mountOptions = [\"umask=0077\"];
-              };
-            };
-            swap = {
-              size = \"$SWAP_SIZE\";
-              content = {
-                type = \"swap\";
-                discardPolicy = \"both\";
-                resumeDevice = true;
-              };
-            };
-            root = {
-              size = \"100%\";
-              content = {
-                type = \"filesystem\";
-                format = \"ext4\";
-                mountpoint = \"/\";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}
-
-
-""" > $HOSTNAME/disks.nix
-
-git add $HOSTNAME
 
 echo "Check the files in $HOSTNAME"
 read -p "Press enter to continue or CTRL+C to abort" READY
-
-
-
 
 echo "Installing..."
 sudo nix --extra-experimental-features nix-command  --extra-experimental-features flakes run github:nix-community/disko/latest#disko-install -- --write-efi-boot-entries --flake .#$HOSTNAME --disk main $DISK
@@ -125,6 +120,7 @@ passwd user
 echo 'set guest password:'
 passwd guest
 chown -R user:users /etc/nixos
+hostfetch
 """
 
 echo "*** Installation Complete! ***"
