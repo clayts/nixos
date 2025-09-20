df10aa2b6831f3f727a2978f007447fe21c0d3a1
clayts <clayton.spicer@gmail.com>
2025-09-20 11:08:01 +0100

    sort out templates a bit

---
+++ common/users/desktop/templates/files/HTML/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = inputs: {
+    devShells =
+      inputs.nixpkgs.lib.genAttrs
+      ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"] (
+        system: let
+          pkgs = import inputs.nixpkgs {inherit system;};
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python313Packages.weasyprint
+            ];
+          };
+        }
+      );
+  };
+}
